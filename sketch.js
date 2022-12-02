0e402ceccdc021b627aec26c07bc2a87ec2d7c19
jiiyeeu <117976654+jiiyeeu@users.noreply.github.com>
2022-12-02 16:04:02 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let r,g,b;
+
+function setup() {
+  createCanvas(640, 480);
+  r = random(50,255);
+  g = random(0,200);
+  b = random(50,255);
+}
+
+function draw() {
+  background(r,g,b);
+  console('draw');
+  } 
+  
+function deviceMoved(){
+  r = map(accelerationX, -90,90,100,175);
+  g = map(accelerationY, -90,90,100,200);
+  b = map(accelerationZ, -90,90,100,200);
+}
+
