221c46e1a7a0d669b5a10c934044f69ce984d06a
jiiyeeu <ywldp@naver.com>
2022-12-17 02:42:48 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+// 기울기
+/*
 let button;
 let permission = false;
 
@@ -13,13 +15,13 @@ function setup() {
     text("is not a ios", 100,100);
   }
 }
-/*  
-function deviceMoved(){
-  r = map(accelerationX, -90,90,100,175);
-  g = map(accelerationY, -90,90,100,200);
-  b = map(accelerationZ, -90,90,100,200);
-}
-*/
+
+///function deviceMoved(){
+///  r = map(accelerationX, -90,90,100,175);
+///  g = map(accelerationY, -90,90,100,200);
+///  b = map(accelerationZ, -90,90,100,200);
+///}
+
 
 function iosAccese(){
   DeviceMotionEvent.requestPermission().then((response) => {
@@ -35,4 +37,31 @@ function draw() {
   textSize(72);
   text(rotationX, 100, 100);
   } 
+  */
 
+// 멀티터치
+
+function setup(){
+  createCanvas(displayWidth, displayHeight)
+}
+
+function draw(){
+
+}
+
+function touchStarted(){
+  background(255);
+  text(touches.length, 200,200);
+  text(touches[0].x,200,200);
+  text(touches[0].y,200,220);
+  text(touches[1].x,200,240);
+  text(touches[1].y,200,280);
+  text(touches[2].x,200,300);
+  text(touches[2].y,200,320);
+  text(touches[3].x,200,340);
+  text(touches[3].y,200,360);
+  text(touches[4].x,200,380);
+  text(touches[4].y,200,400);
+  text(touches[5].x,200,420);
+  text(touches[5].y,200,440);
+}
