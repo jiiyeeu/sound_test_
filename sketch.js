0100932e311ef74a0359c7a86fa6923c072e02b2
jiiyeeu <ywldp@naver.com>
2022-12-17 02:10:37 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,37 @@
-let r,g,b;
+let button;
+let permission = false;
 
 function setup() {
-  createCanvas(640, 480);
-  //r = random(50,255);
-  g = random(0,200);
-  b = random(50,255);
-}
+  createCanvas(windowWidth, windowHeight);
 
-function draw() {
-  background(20,g,b);
-  console('draw');
-  } 
+  if(typeof DeviceMotionEvent.requestPermission === "function"){
+    background(255,0,0);
+    button = createButton("Click to iOS Sensor");
+    button.mousePressed(iosAccese);
+  }else{
+    background(0,255,0);
+    text("is not a ios", 100,100);
+  }
+}
   
 function deviceMoved(){
   r = map(accelerationX, -90,90,100,175);
   g = map(accelerationY, -90,90,100,200);
   b = map(accelerationZ, -90,90,100,200);
 }
+
+function iosAccese(){
+  DeviceMotionEvent.requestPermission().then((response) => {
+    if(response === "granted"){
+      permission = true;
+    }
+  }).catch(console.error);
+} 
+
+function draw() {
+  if(!permission) return;
+  background(255,255,255);
+  textSize(72);
+  text(rotationX, 100, 100);
+  } 
 
